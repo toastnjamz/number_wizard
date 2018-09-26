b75ee9aaa52309ada604adc00982b47d28c9cd8f
Verity Davis <toastnjamz@users.noreply.github.com>
2018-09-25 21:50:42 -0700

    Update numberwizard.cs

--- numberwizard.cs
+++ numberwizard.cs
@@ -17,15 +17,15 @@ public class NumberWizard : MonoBehaviour {
 		min = 1;
 		guess = 500;
 		
-		print ("========================");
-		print ("Welcome to Number Wizard");
-		print ("Pick a number in your head, but don't tell me.");
+		Debug.Log("========================");
+		Debug.Log("Welcome to Number Wizard");
+		Debug.Log("Pick a number in your head, but don't tell me.");
 		
-		print ("The highest number you can pick is " + max);
-		print ("The lowest number you can pick is " + min);
+		Debug.Log("The highest number you can pick is " + max);
+		Debug.Log("The lowest number you can pick is " + min);
 		
-		print ("Is the number higher or lower than" + guess);
-		print ("Up Arrow for higher, Down Arrow for lower, Return for equals");
+		Debug.Log("Is the number higher or lower than" + guess);
+		Debug.Log("Up Arrow for higher, Down Arrow for lower, Return for equals");
 		
 		max = max + 1;
 	}
@@ -33,22 +33,22 @@ public class NumberWizard : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			// print("Up Arrow pressed");
+			//Debug.Log("Up Arrow pressed");
 			min = guess;
 			NextGuess ();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			// print("Down Arrow pressed");
+			// Debug.Log("Down Arrow pressed");
 			max = guess;
 			NextGuess ();
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
-			print("I won");
+			Debug.Log("I won");
 			StartGame ();
 		}				
 	}
 	
 	void NextGuess () {
 		guess = (max + min) / 2;
-		print ("Higher or lower than" + guess);
-		print ("Up Arrow for higher, Down Arrow for lower, Return for equals");
+		Debug.Log("Higher or lower than" + guess);
+		Debug.Log("Up Arrow for higher, Down Arrow for lower, Return for equals");
 	}
 }
