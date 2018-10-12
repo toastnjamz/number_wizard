50a92a40ff224514857e1cf8954808f81328e1e8
Verity Davis <toastnjamz@users.noreply.github.com>
2018-10-11 20:27:43 -0700

    Update numberwizard.cs

--- numberwizard.cs
+++ numberwizard.cs
@@ -1,54 +1,56 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-	// Use this for initialization
-	int max;
-	int min;
-	int guess;
-	
-	void Start () {
-		StartGame ();
-	}
-	
-	void StartGame () {
-		max = 1000;
-		min = 1;
-		guess = 500;
-		
-		Debug.Log("========================");
-		Debug.Log("Welcome to Number Wizard");
-		Debug.Log("Pick a number in your head, but don't tell me.");
-		
-		Debug.Log("The highest number you can pick is " + max);
-		Debug.Log("The lowest number you can pick is " + min);
-		
-		Debug.Log("Is the number higher or lower than" + guess);
-		Debug.Log("Up Arrow for higher, Down Arrow for lower, Return for equals");
-		
-		max = max + 1;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			//Debug.Log("Up Arrow pressed");
-			min = guess;
-			NextGuess ();
-		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			// Debug.Log("Down Arrow pressed");
-			max = guess;
-			NextGuess ();
-		} else if (Input.GetKeyDown(KeyCode.Return)) {
-			Debug.Log("I won");
-			StartGame ();
-		}				
-	}
-	
-	void NextGuess () {
-		guess = (max + min) / 2;
-		Debug.Log("Higher or lower than" + guess);
-		Debug.Log("Up Arrow for higher, Down Arrow for lower, Return for equals");
-	}
+    int max;
+    int min;
+    int guess;
+
+    // Use this for initialization
+    void Start () 
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I win!");
+            StartGame();
+        }
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than" + guess);
+    }
 }
